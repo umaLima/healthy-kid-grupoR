cac63b0d31cab8b8809b8970ccb606f75453702a
umaLima <oumaimajidi26@gmail.com>
2025-12-02 15:38:35 +0100

    Proyecto inicial Spring Boot

---
+++ src/main/java/com/grupoR/springboot_githubflow/SpringbootGithubflowApplication.java
@@ -0,0 +1,13 @@
+package com.grupoR.springboot_githubflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupoR/springboot_githubflow/SpringbootGithubflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoR.springboot_githubflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
