39600177e4801862fd7d59e8464de4c5286f93d5
ghitaelmra <ghitaaelmra@gmail.com>
2025-12-03 15:47:10 +0100

    Añadir GhitaController

---
+++ src/main/java/com/grupoR/springboot_githubflow/GhitaController.java
@@ -0,0 +1,13 @@
+package com.grupoR.springboot_githubflow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GhitaController {
+
+    @GetMapping("/ghita")
+    public String saludo() {
+        return "Hola, soy Ghita!";
+    }
+}
